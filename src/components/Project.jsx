db6add15c1dd1b2ac3ffd898b84f226a62cbedcb
AyaMouawi <ayamouawi8@gmail.com>
2023-09-28 10:08:05 +0300

    Testing Branches

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,16 +3,16 @@ import styled from "styled-components";
 import "../css/Project.css"
 
 
-const Project = ({ imageUrl, title, description1, description2 }) => {
+const Project = ({ imageUrl, title, description, description2 }) => {
   return (
     
     
     <div className="ProjectContainer">
       <img className="ProjectImage" src="../../Images/Projectpic.png" alt="{title}" />
-      <h2 className="ProjectTitle">Project Name</h2>
+      <h2 className="ProjectTitle">{title}</h2>
       <hr class="CustomHR" />
       <div className="ProjectDetails">
-      <p className="ProjectDescription1">The website is about a honey product store, u can check all products, add items to your cart and simply order them!</p>
+      <p className="ProjectDescription1">{description}</p>
       <p className="ProjectDescription2">Tech used: MERN stack, Html, Css, TailwindCss, Javascript, React, Node, Express, Mongoose, MongoDb, Docker</p>
       <div className="ButtonWrapper">
         <button className="ProjectDemoButton">Live Demo</button>
