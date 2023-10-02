fd2c32b57995486dc086b568a55f9bd9f88653dd
Shirineahmad <shirine.ahmad@hotmail.com>
2023-10-02 09:56:11 +0300

    hero responsive

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import '../css/HeroSection.css';
 
 const HeroSection = ({ SmallDesc, HeroImage }) => {
   return (
-    <div className="head-section-2">
+    <div className="head-section2">
       <div className="herosection3">
         <div className="helo">
           <p className="intro">Hello!</p>
@@ -12,8 +12,36 @@ const HeroSection = ({ SmallDesc, HeroImage }) => {
           <button className="btn_start">Get to Know Me</button>
         </div>
         <div className="cont-img">
-          <img className="image1" src="/Images/HeroBackground.png" alt="Rectangle" />
-          <img className="image2" src={`data:image/png;base64,${HeroImage}`} alt="Raoul" />
+          <img
+            className="image1"
+            src="/Images/HeroBackground.png"
+            alt="Rectangle"
+          />
+          <img
+            className="image2"
+            src={`data:image/png;base64,${HeroImage}`}
+            alt="Raoul"
+          />
+        </div>
+      </div>
+      <div className="heroSection3-responsive">
+        <p className="intro1">Hello!</p>
+        <p className="head-title">I'm Raoul Baddawi</p>
+        <p className="intro">{SmallDesc}</p>
+        <div className="cont-img">
+          <img
+            className="image1"
+            src="/Images/HeroBackground.png"
+            alt="Rectangle"
+          />
+
+          <button className="btn_start">Get to Know Me</button>
+          <img
+            className="image2"
+            src={`data:image/png;base64,${HeroImage}`}
+            alt="Raoul"
+          />
+        
         </div>
       </div>
     </div>
